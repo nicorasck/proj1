f33fd81cd165faf2a6e31551622552ec260f17a3
nicholas <nicholas.rasck@gmail.com>
2024-08-30 11:20:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace proj1;
+
+class Program
+    {
+    static void Main(string[] args)
+        {
+             Console.WriteLine("Program begin");
+
+            int sum = 0;
+
+            for (int i = 0; i <= 3; i++)
+            {
+                sum += i;
+                Console.WriteLine($"i = {i}");
+
+                if (i == 3)
+                {
+                    Console.WriteLine("We have now the magical sum of 3");
+                }
+            }
+            Console.WriteLine("Program end.");
+        }
+    }
+
+
